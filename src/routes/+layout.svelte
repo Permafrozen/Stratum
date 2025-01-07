5b1308751f86a0e95c33737d93c3a5e90a6e26f6
Permafrozen <m8tt8o@gmail.com>
2025-01-07 11:41:32 +0100

    added shared layout for nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+  let { children } = $props();
+</script>
+
+<nav>
+  <a href="{base}/">home</a> <br />
+  <a href="{base}/dotfiles">dotfiles</a> <br />
+  <a href="{base}/presentations">presentations</a> <br />
+</nav>
+
+{@render children()}
